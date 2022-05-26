affcde94b9414774880a8089ae1c7444a3067cdb
Szymon Krolik <krolik.sz@wp.pl>
2022-05-26 14:42:57 +0200

    Get all coupons by restaoarnt id

--- src/main/java/com/example/restaurantapi/dto/cupon/CreatedCuponDto.java
+++ src/main/java/com/example/restaurantapi/dto/cupon/CreatedCuponDto.java
@@ -8,13 +8,14 @@ public class CreatedCuponDto {
     String restaurantName;
     double value;
     int maxUse;
+    int id;
 
     public static CreatedCuponDto of(Cupon dto) {
         CreatedCuponDto coupon = new CreatedCuponDto();
         coupon.setMaxUse(dto.getMaxUses());
         coupon.setValue(dto.getValue());
         coupon.setRestaurantName(dto.getRestaurant().getName());
-
+        coupon.setId(dto.getId());
         return coupon;
     }
 }
